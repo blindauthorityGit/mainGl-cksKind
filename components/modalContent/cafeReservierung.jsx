bc4c17e7703cc9528b7710a94998b991e2d286bb
Johannes <office@atelierbuchner.at>
2024-01-26 18:35:23 +0100

    cafe

--- components/modalContent/cafeReservierung.jsx
+++ components/modalContent/cafeReservierung.jsx
@@ -1,13 +1,62 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import DatePicker from "react-datepicker";
 import "react-datepicker/dist/react-datepicker.css";
 import { H4, P } from "../typography";
 import { MainButtonNOLink } from "../buttons";
 
+// FIREBASE
+import { fetchFirestoreData } from "../../config/firebase";
+const TIME_SLOTS = ["09:30 - 11:30", "11:30 - 13:30"];
+
 const CafeReservierung = () => {
     const [startDate, setStartDate] = useState(new Date());
     const [time, setTime] = useState("");
     const [guests, setGuests] = useState(1);
+
+    const [availableTimeSlots, setAvailableTimeSlots] = useState([]);
+    const [isFullyBooked, setIsFullyBooked] = useState(false);
+
+    const MAX_CAPACITY = 32;
+
+    const checkAvailability = async (selectedDate, numberOfGuests) => {
+        setIsFullyBooked(false);
+        const formattedDate = selectedDate.toISOString().split("T")[0];
+
+        try {
+            const reservations = await fetchFirestoreData("reservierung_cafe");
+            const reservationsOnDate = reservations.filter((reservation) => {
+                const reservationDate = new Date(reservation.datum.seconds * 1000).toISOString().split("T")[0];
+                return reservationDate === formattedDate;
+            });
+
+            const availableSlotsWithSpaces = TIME_SLOTS.map((slot) => {
+                const totalGuestsInSlot = reservationsOnDate
+                    .filter((reservation) => reservation.zeit === slot)
+                    .reduce((total, current) => total + current.anzahl, 0);
+
+                return {
+                    slot: slot,
+                    availableSpaces: MAX_CAPACITY - totalGuestsInSlot,
+                };
+            }).filter((slot) => slot.availableSpaces >= numberOfGuests);
+
+            setAvailableTimeSlots(availableSlotsWithSpaces);
+            setIsFullyBooked(availableSlotsWithSpaces.length === 0);
+        } catch (error) {
+            console.error("Error checking availability:", error);
+            // Handle error
+        }
+    };
+
+    useEffect(() => {
+        fetchFirestoreData("reservierung_cafe")
+            .then((data) => {
+                console.log(data);
+            })
+            .catch((error) => {
+                console.error("Error fetching data:", error);
+            });
+    }, []);
 
     const handleSubmit = async (event) => {
         event.preventDefault();
@@ -16,39 +65,78 @@ const CafeReservierung = () => {
         // Send this data to the backend and handle the response
     };
 
+    const isWeekday = (date) => {
+        const day = date.getDay();
+        return day !== 0 && day !== 6; // 0 is Sunday and 6 is Saturday
+    };
+
     return (
         <div className="container mx-auto grid grid-cols-12">
-            <div className="col-span-12">
+            <div className="col-span-12 xl:col-span-6">
                 <H4 klasse="mt-4 mb-6 !text-xl">Reservierung!</H4>
 
                 <form onSubmit={handleSubmit}>
+                    <label htmlFor="date-picker" className="block text-sm font-medium text-gray-700 mb-1">
+                        Datum auswählen:
+                    </label>
                     <DatePicker
-                        className="col-span-12 text-xs  border-2 rounded-full border-textColor bg-transparent text-textColor placeholder-primaryColor-950 font-sans p-2 sm:p-4"
+                        id="date-picker"
+                        className="col-span-12 text-xs  border-2 rounded-full border-textColor bg-transparent text-textColor placeholder-primaryColor-950 font-sans p-2 sm:p-4 mb-4"
                         selected={startDate}
-                        onChange={(date) => setStartDate(date)}
-                        dateFormat="yyyy/MM/dd"
+                        onChange={(date) => {
+                            setStartDate(date);
+                            setAvailableTimeSlots([]);
+                            setIsFullyBooked(false);
+                        }}
+                        dateFormat="dd/MM/yyyy"
+                        dayClassName={(date) => (isWeekday(date) ? "weekday bg-primaryColor-200" : "")}
                     />
-                    <select value={time} onChange={(e) => setTime(e.target.value)}>
-                        {/* Generate time slots here */}
-                    </select>
-                    <input
-                        className="col-span-12 text-xs  border-2 rounded-full border-textColor bg-transparent text-textColor placeholder-primaryColor-950 font-sans p-2 sm:p-4"
-                        type="number"
-                        value={guests}
-                        onChange={(e) => setGuests(e.target.value)}
-                        min="1"
-                        max="32"
-                    />
+                    {startDate && (
+                        <>
+                            <label htmlFor="guest-number" className="block text-sm font-medium text-gray-700 mb-1">
+                                Anzahl der Gäste:
+                            </label>
+                            <input
+                                id="guest-number"
+                                className="col-span-12 text-xs border-2 rounded-full border-textColor bg-transparent text-textColor placeholder-primaryColor-950 font-sans p-2 sm:p-4"
+                                type="number"
+                                value={guests}
+                                onChange={(e) => {
+                                    setGuests(e.target.value);
+                                    if (e.target.value && startDate) {
+                                        checkAvailability(startDate, e.target.value);
+                                    }
+                                }}
+                                min="1"
+                                max="32"
+                            />
+                        </>
+                    )}
+                    {guests && availableTimeSlots.length > 0 && (
+                        <>
+                            <label htmlFor="time-select" className="block text-sm font-medium text-gray-700 mb-1">
+                                Zeitfenster wählen:
+                            </label>
+                            <select
+                                id="time-select"
+                                value={time}
+                                onChange={(e) => setTime(e.target.value)}
+                                className="mb-4"
+                            >
+                                {availableTimeSlots.map(({ slot, availableSpaces }, index) => (
+                                    <option key={index} value={slot}>
+                                        {slot} (Verfügbare Plätze: {availableSpaces})
+                                    </option>
+                                ))}
+                            </select>
+                        </>
+                    )}
+                    <div className="w-full col-span-12 sm:mb-8">
+                        <MainButtonNOLink type="submit" klasse="bg-primaryColor border-2 border-primaryColor mt-4">
+                            Absenden
+                        </MainButtonNOLink>
+                    </div>
                 </form>
-                <div className="w-full col-span-12 sm:mb-8">
-                    <MainButtonNOLink
-                        // className="bg-primaryColor-500 text-white mt-6 tracking-widest hover-underline-animation z-20 flex items-center justify-center lg:mt-8 py-2 text-sm sm:text-base sm:py-3 px-6 min-w-[10rem] w-full uppercase rounded-md md:mt-8"
-                        type="submit"
-                        klasse="bg-primaryColor border-2 border-primaryColor mt-4"
-                    >
-                        Absenden
-                    </MainButtonNOLink>
-                </div>
             </div>
         </div>
     );
---
+++ config/firebase.js
@@ -0,0 +1,32 @@
+import { initializeApp } from "firebase/app";
+import { getFirestore, collection, getDocs, doc, setDoc, addDoc } from "firebase/firestore/lite";
+import { getStorage, ref, uploadBytes, listAll, getDownloadURL } from "firebase/storage";
+
+const firebaseConfig = {
+    apiKey: process.env.NEXT_FIREBASE,
+    authDomain: "mainglueckskind-a4d01.firebaseapp.com",
+    projectId: "mainglueckskind-a4d01",
+    storageBucket: "mainglueckskind-a4d01.appspot.com",
+    messagingSenderId: "736888938250",
+    appId: "1:736888938250:web:2b8c5a0a38220106d264f3",
+    measurementId: "G-47QJ8EXBB9",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const db = getFirestore(app);
+const storage = getStorage(app);
+
+export { app, db, storage };
+
+export const fetchFirestoreData = async (collectionName) => {
+    try {
+        const querySnapshot = await getDocs(collection(db, collectionName));
+        const data = querySnapshot.docs.map((doc) => ({ id: doc.id, ...doc.data() }));
+        console.log(data);
+        return data;
+    } catch (error) {
+        console.error("Error fetching data:", error);
+        return [];
+    }
+};
