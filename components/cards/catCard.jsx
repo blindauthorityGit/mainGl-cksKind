be5fae93c15a7c5cf85a96eb70aa417369b5a0e3
Johannes <office@atelierbuchner.at>
2024-05-29 19:29:23 +0200

    test redesign branch

--- components/cards/catCard.jsx
+++ components/cards/catCard.jsx
@@ -5,12 +5,12 @@ const CatCard = ({ bgColor, icon, text, link, isWhite }) => {
     console.log(icon);
     return (
         <div
-            className={`bg-[${bgColor}] h-[17vh] rounded-[10px] flex flex-col justify-center items-center font-sans font-semibold text-text`}
+            className={`bg-[${bgColor}] h-[17svh] rounded-[10px] flex flex-col justify-center items-center font-sans font-semibold text-text`}
             style={{ backgroundColor: bgColor }}
         >
             {icon && <img src={icon} alt="icon" />}
             <P
-                klasse={`font-semibold text-[clamp(12px,calc(12px+0.00893*(100vw-320px)),16px)] ${
+                klasse={`font-semibold text-[clamp(12px,calc(12px+0.00893*(100svw-320px)),16px)] ${
                     isWhite ? "!text-white" : null
                 }`}
             >
